89ff71c1383c3c5994af2873d4da26053a73deaf
Lucas <lucasoliveirars22@gmail.com>
2025-02-09 12:26:31 -0300

    :sparkles: creating test e2e to create course and login

---
+++ cypress/e2e/create_course.cy.js
@@ -0,0 +1,64 @@
+describe("Cadastro de Curso", () => {
+  const username = "grupo3-coordenador@gmail.com";
+  const password = "123deoliveira4";
+
+  beforeEach(() => {
+    cy.visit("/");
+
+    // Fazer login
+    cy.get('input[id="id_login"]').type(username);
+    cy.get('input[id="id_password"]').type(password);
+    cy.get("button").contains("Entrar").click();
+    cy.url().should("contain", "/");
+
+    cy.contains("a", "Acadêmico").click({ force: true });
+    cy.contains("div", "Cursos").click({ force: true });
+    cy.contains("a", "Cadastrar Novo Curso").click({ force: true });
+  });
+
+  it("Deve preencher e enviar o formulário corretamente", () => {
+    cy.get("#id_name").type("Engenharia de Software");
+    cy.get("#id_period").select("Matutino");
+    cy.get("#id_duration_type").select("Semestral");
+    cy.get("#id_course_type").select("Pós-Graduação");
+    cy.get("#id_number_of_periods").type("8");
+
+    cy.get("button").contains("Salvar").click();
+
+    cy.contains("O curso foi cadastrado com sucesso.").should("be.visible");
+  });
+
+  it("Deve exibir erro ao tentar enviar o formulário vazio", () => {
+    cy.get("button").contains("Salvar").click();
+
+    cy.get("article[role='alert']").should("be.visible");
+
+    cy.contains("p", "Nome: Este campo é obrigatório.").should("be.visible");
+    cy.contains("p", "Turno: Este campo é obrigatório.").should("be.visible");
+    cy.contains("p", "Tipo: Este campo é obrigatório.").should("be.visible");
+    cy.contains("p", "Tipo de duração: Este campo é obrigatório.").should(
+      "be.visible"
+    );
+    cy.contains(
+      "p",
+      "Número de períodos/Anos: Este campo é obrigatório."
+    ).should("be.visible");
+  });
+
+  // DEU CERTO PRA CRIAR MESMO COM CARACTERES ESPECIAIS NO NOME (REPORTAR)
+  it("Testando caracteres especiais no campo nome", () => {
+    cy.get("#id_name").type("@!123##");
+    cy.get("#id_period").select("Matutino");
+    cy.get("#id_duration_type").select("Semestral");
+    cy.get("#id_course_type").select("Pós-Graduação");
+    cy.get("#id_number_of_periods").type("8");
+
+    cy.get("button").contains("Salvar").click();
+
+    cy.get("article[role='alert']").should("be.visible");
+    cy.contains(
+      "p",
+      "Nome: O campo não deve conter caracteres especiais."
+    ).should("be.visible");
+  });
+});
